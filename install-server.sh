fe7b923c01b7606aee398eced64ef8f04d416638
Alex Nguetcha <nguetchaalex@gmail.com>
2024-03-30 00:10:40 +0100

    Upgrading to php 8.1

--- install-server.sh
+++ install-server.sh
@@ -1,73 +1,61 @@
 #!/usr/bin/env bash
 
-#update system dependencies
+# Update system dependencies
 apt-get update -y
 apt-get upgrade -y
 
-#install nginx
+# Install nginx
 apt-get install nginx nginx-extras -y
 systemctl start nginx.service
 systemctl enable nginx
 
-#installing php
+# Installing PHP and its extensions
 apt-get install php -y
-
-#installing basic extension for php
 apt-get install php-{bcmath,bz2,intl,gd,mbstring,mysql,zip,fpm} -y
 
-#installing symfony php extension requirement
-apt install php libapache2-mod-php php-mbstring php-xmlrpc php-soap php-gd php-xml php-cli php-zip php-mysql php-curl php-opcache php-xml curl git -y
-apt-get install php-fpm php-mbstring php-opcache php-xml php-zip php-curl php-mysql php-cli curl git -y
+# Add repository for PHP 8.1
+add-apt-repository ppa:ondrej/php -y
+apt-get update
 
-apt-get install software-properties-common
+# Install PHP 8.1
+apt-get install php8.1 php8.1-cli php8.1-common -y
 
-#go to php 7.3
-LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
-apt update
-apt install php7.3 php7.3-cli php7.3-common -y
+# Install PHP 8.1 extensions
+apt-get install php8.1-{curl,dev,gd,mbstring,zip,mysql,xml,fpm,imagick,xmlrpc,intl} -y
 
-#installing extensions for 7.3
-apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm
-apt install libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy -y
-apt install php7.3-xmlrpc php7.3-intl php7.3-mbstring php7.3-gd -y
+# Set PHP 8.1 as default version
+update-alternatives --set php /usr/bin/php8.1
 
-#setting php 7.3 as default level
-update-alternatives --set php /usr/bin/php7.3
+# Install imagemagick
+apt-get install imagemagick -y
 
-#install imagemagick
-apt install imagemagick -y
-#Installing Imagick PHP Extension
-apt install php-imagick -y
-#check imagick configurations
+# Installing Imagick PHP Extension
+apt-get install php-imagick -y
+
+# Check Imagick configurations
 php-config --extension-dir
-#add extension=imagick at end of
-echo "extension=imagick" >> /etc/php/7.3/fpm/php.ini
 
+# Add extension=imagick at end of php.ini
+echo "extension=imagick" >> /etc/php/8.1/fpm/php.ini
+
+# Restart Nginx
 systemctl restart nginx
 
-#Just for projects which use PHP and composer packages
-#installing composer for symfony php
+# Install Composer
 wget https://getcomposer.org/installer
 php installer
-#making composer global
 mv composer.phar /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
 
+# Install MySQL
+apt-get install mysql-server -y
+service mysql stop
 
-#installing mysql
-apt install mysql-server -y
-#stop mysql service
-sudo service mysql stop
-
-#solving issue of restart without grant
-sudo mkdir -p /var/run/mysqld
-sudo chown -R mysql:mysql /var/run/mysqld
-sudo chown -R mysql:mysql /var/lib/mysql
-
-sudo mysqld_safe --skip-grant-tables &
+# Solve issue of restart without grant
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+mysqld_safe --skip-grant-tables &
 
 echo "After prompt Type lines into sql-user-update.sh (line by line)"
-
 mysql -u root
-#Type lines into sql-user-update.sh (line by line)
-
